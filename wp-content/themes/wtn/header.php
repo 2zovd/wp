e05570c3fc7c709b2239a8122f5a7811235592d6
Tuzov <dmy2zov@gmail.com>
2017-03-28 17:59:52 +0300

    wp

---
+++ wp-content/themes/wtn/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+	$template_path = get_template_directory_uri();
+ ?>
+<head>
+	<meta charset="UTF-8">
+	<title>wtn</title>
+
+</head>
+<body>
+
+	<link rel="stylesheet" href="<?php echo $template_path ?>/style.css">
+	<script src="<?php echo $template_path ?>/js/common.js"></script>
+</body>
+</html>
